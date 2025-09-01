477d42a851717b5f0f1a6778bc1b2301919e7112
d36351 <yulun1119@gmail.com>
2025-09-01 21:39:21 +0800

    Site updated: 2025-09-01 21:39:21

---
+++ dist/music.js
@@ -0,0 +1,36 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: false,
+    autoplay: false,
+    theme: '#b7daff',
+    loop: 'all',
+    order: 'list',
+    preload: 'auto',
+    volume: 0.7,
+    mutex: true,
+    listFolded: false,
+    listMaxHeight: '200px',
+    lrcType: 3,
+    audio: [
+      {
+        name: "DevourerofGodsPhase2",
+        artist: 'CalamityTeam',
+        url: 'https://www.youtube.com/watch?v=OspSOSPjOuI',
+        cover: 'C:\\Users\\user\\Pictures\\artworks-75PtKCIxlzOa6oDi-4CcoVw-t500x500.jpg',
+        lrc: '',
+      },
+      {
+        name: 'DevourerofGodsPhase2',
+        artist: 'CalamityTeam',
+        url: 'https://XXX.mp3',
+        cover: 'C:\\Users\\user\\Pictures\\artworks-75PtKCIxlzOa6oDi-4CcoVw-t500x500.jpg',
+      },
+      {
+        name: 'DevourerofGodsPhase2',
+        artist: 'CalamityTeam',
+        url: 'https://www.youtube.com/watch?v=OspSOSPjOuI',
+        cover: 'C:\\Users\\user\\Pictures\\artworks-75PtKCIxlzOa6oDi-4CcoVw-t500x500.jpg',
+        lrc: ''
+      }
+    ]
+});
